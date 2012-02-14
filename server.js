5c92fb15a01ed8176f29ecb0f3af59bd016aab34
Ole Petter Bang <olepbang@gmail.com>
2012-02-14 21:47:36 +0100

    Refactor.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var http = require('http')
   , fs = require('fs')
   , EventEmitter = require('events').EventEmitter
   , emitter = new EventEmitter()
+  , util = require('util')
   ;
   
 http.createServer(handleRequest).listen(5000);
@@ -11,44 +12,62 @@ http.createServer(handleRequest).listen(5000);
 function handleRequest(req, res) {
   var path = url.parse(req.url).pathname
     , method = req.method
-    , body = ''
-    , post
     ;
 
   if (path === '/capture') {
-    fs.readFile('capture.html', function (err, data) {
-      res.setHeader('Content-type', 'text/html');
-      res.end(data);
-    });
+    captureBrowser(req, res);
   }
   else if (path === '/test' && method === 'POST') {
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      emitter.emit('test', body);
-      emitter.on('result', function (result) {
-        res.end(result);
-      });
-    });
+    scheduleTest(req, res);
   }
   else if (path === '/run') {
-    emitter.on('test', function (test) {
-      res.setHeader('Content-type', 'text/json');
-      res.end(JSON.stringify({test: test}));
-    });
+    awaitTest(req, res);
   }
   else if (path === '/result') {
-    req.on('data', function (data) {
-      body += data;
-    });
-    req.on('end', function () {
-      emitter.emit('result', body)
-      res.end();
-    });
+    notifyResult(req, res);
   }
   else {
     res.writeHead(404);
     res.end();
   }
+}
+
+function captureBrowser(req, res) {
+  fs.readFile('capture.html', function (err, data) {
+    res.setHeader('Content-type', 'text/html');
+    res.end(data);
+  });
+}
+
+function scheduleTest(req, res) {
+  var body = '';
+
+  req.on('data', function (data) {
+    body += data;
+  });
+  req.on('end', function () {
+    emitter.emit('test', body);
+    emitter.on('result', function (result) {
+      res.end(result);
+    });
+  });
+}
+
+function awaitTest(req, res) {
+  emitter.on('test', function (test) {
+    res.setHeader('Content-type', 'text/json');
+    res.end(JSON.stringify({test: test}));
+  });
+}
+
+function notifyResult(req, res) {
+  var body = '';
+
+  req.on('data', function (data) {
+    body += data;
+  });
+  req.on('end', function () {
+    emitter.emit('result', body)
+    res.end();
+  });
 }
